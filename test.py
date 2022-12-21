d14345c6ac2bea61e46a28d55e202de79243834f
Alistair Vu <alistairvu@gmail.com>
2022-12-21 13:40:36 +0700

    Delete redundant files

--- test.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-img = Image.open('irene_test.jpeg')
-width, height = img.size
-img.thumbnail((width // 2, height // 2), Image.Resampling.LANCZOS)
-img.save('mini_irene.jpeg')
